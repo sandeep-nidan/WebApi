a55761033f872ea39a32dba67fb832fba38bbfa1
Sandeep <sandeepp.nidan@gmail.com>
2022-07-01 15:43:17 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { UsersDataService } from './services/users-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebApi';
+  users: any;
+  constructor(private userData: UsersDataService) {
+    userData.users().subscribe((data) => {
+      console.warn("data", data);
+      this.users = data
+    });
+
+  }
+  getUserFromData(data: any) 
+  {
+    this.userData.saveUsers(data).subscribe((result)=>{
+      console.warn(result)
+    })
+    
+  }
 }
